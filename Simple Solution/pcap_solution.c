bbd705ca7e60efb69b9a05d6aa9c661d4a700886
Herat <hag59@cornell.edu>
2013-10-30 17:12:02 -0400

    Eth modified

--- Simple Solution/pcap_solution.c
+++ Simple Solution/pcap_solution.c
@@ -8,25 +8,77 @@
 #include <net/ethernet.h>
 #include <netinet/ip.h>
 
-void process_packet(u_char *, const struct pcap_pkthdr *,
+#define INT1 "eth0"
+#define INT2 "eth1"
+#define MAC1_1 0
+#define MAC1_2 12
+#define MAC1_3 41
+#define MAC1_4 145
+#define MAC1_5 135
+#define MAC1_6 100
+#define MAC2 "00:0c:29:91:87:a4" 
+
+void process_packet_in(u_char *, const struct pcap_pkthdr *,
 		const u_char *);
 
 pcap_t * in_handle;
 pcap_t * out_handle;
 FILE* fp;
+int mac_t[6];
+
+void getArrayFromString(char* str1)
+{
+	char c = ' ';
+	int i = 0;
+	int total = 0;
+	int pos = 16;
+	int j = 0;
+	char v;
+	
+	while((c = str1[i]) != '\0')
+	{
+		if(c == ':')
+		{
+			pos = 16;
+			mac_t[j] = total;
+			total = 0;
+			j++;
+		}
+		else
+		{
+			if(c == 'a' || c == 'A')
+				v = 10;
+			else if(c == 'b' || c == 'B')
+				v = 11;
+			else if(c == 'c' || c == 'C')
+				v = 12;		
+			else if(c == 'd' || c == 'D')
+				v = 13;
+			else if(c == 'e' || c == 'E')
+				v = 14;
+			else if(c == 'f' || c == 'F')
+				v = 15;
+			else
+				v = c - 48;
+			total += pos * v;
+			pos = 1;
+		}
+		i++;
+	}
+}
  
 int main(int argc,char*argv[])
 {
     char errbuf[100], *devname;
     int packets;
 
-    in_handle = pcap_open_live("eth0",65536,1,0,errbuf);
-    out_handle = pcap_open_live("eth1",65536,1,0,errbuf);
+    in_handle = pcap_open_live(INT1,65536,1,0,errbuf);
+    out_handle = pcap_open_live(INT2,65536,1,0,errbuf);
     
     fp = fopen("rules","r");
     
     pcap_loop(in_handle, -1, process_packet_in, NULL);
-    pcap_loop(out_handle, -1, process_packet_out, NULL);
+    //pcap_loop(out_handle, -1, process_packet_out, NULL);
     
     fclose(fp);
     return 0;
@@ -35,11 +87,13 @@ int main(int argc,char*argv[])
 void process_packet_in(u_char *args, const struct pcap_pkthdr *header,
 		const u_char *buffer)
 {
-	unsigned short iphdrlen;
+	struct ethhdr *eth = (struct ethhdr *)buffer;
+    unsigned short iphdrlen;
     struct iphdr *iph = (struct iphdr *)(buffer +
     		sizeof(struct ethhdr) );
 	struct sockaddr_in source,dest;
     iphdrlen = iph->ihl*4;
+    
     //Get the source IP address
     memset(&source, 0, sizeof(source));
 	source.sin_addr.s_addr = iph->saddr;
@@ -51,12 +105,24 @@ void process_packet_in(u_char *args, const struct pcap_pkthdr *header,
 	char* src_ip_s = inet_ntoa(source.sin_addr);
 	char* dst_ip_s = inet_ntoa(dest.sin_addr);
 	
-	if(strcmp(src_ip_s,"192.168.48.152") == 0) {
+	int i;
+	/*getArrayFromString(MAC2);
+	for(i=0;i<6;i++)
+		printf("%d\n",mac_t[i]);*/
+	
+	eth->h_source[0] = MAC1_1;
+	eth->h_source[1] = MAC1_2;
+	eth->h_source[2] = MAC1_3;
+	eth->h_source[3] = MAC1_4;
+	eth->h_source[4] = MAC1_5;
+	eth->h_source[5] = MAC1_6;
+	
+	//if(strcmp(src_ip_s,"192.168.48.152") == 0) {
 		printf("%d\n",pcap_inject(out_handle,buffer,header->len));
-	}
+	//}
 }
 
-void process_packet_out(u_char *args, const struct pcap_pkthdr *header,
+/*void process_packet_out(u_char *args, const struct pcap_pkthdr *header,
 		const u_char *buffer)
 {
 	unsigned short iphdrlen;
@@ -78,4 +144,4 @@ void process_packet_out(u_char *args, const struct pcap_pkthdr *header,
 	if(strcmp(src_ip_s,"192.168.48.152") == 0) {
 		printf("%d\n",pcap_inject(out_handle,buffer,header->len));
 	}
-}
+}*/
